4088fccfd48f88c81786adf8d80ec278a227b284
Dalton <n7dalton@gmail.com>
2023-04-05 08:58:52 -0500

    ur mom update

--- Methods_Lumpking_Dalton/Program.cs
+++ Methods_Lumpking_Dalton/Program.cs
@@ -6,31 +6,42 @@ namespace Methods_Lumpking_Dalton
     {
         static void Main(string[] args)
         {
+            //asks them the question
             Console.WriteLine("would you like to find a new number(n) or come into my house?(h)");
+            //sets twoOptions to what they say
             string twoOptions = Console.ReadLine();
+            //checks if they answered N or H
             if (twoOptions == "n")
             {
                 Numbers();
             }
             if (twoOptions == "h")
             {
-                enterHouse();
+                EnterHouse();
             }
-
-            void enterHouse()
+            //this is the method
+            void EnterHouse()
             {
+                //asks name
                 Console.WriteLine("Whats your name??");
+                //declares name
                 string name = Console.ReadLine();
                 Console.WriteLine($"Welcome {name} to my lovely home!");
                 Console.WriteLine("now go away.");
             }
             void Numbers()
             {
+                //asks for age
                 Console.WriteLine("whats your age");
+                //declares age
                 int Age = Convert.ToInt32(Console.ReadLine());
+                //asks for their fav number
                 Console.WriteLine("whats your fav number?");
+                //declares favNum
                 int favNum = Convert.ToInt32(Console.ReadLine());
+                //does sum dumb math crap
                 int multipliedNum = Age * favNum;
+                //tells them to get out of the coffee shop
                 Console.WriteLine($"your new lucky number is....{multipliedNum}!! now get out of my coffee shop.");
             }
            
