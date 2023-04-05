12fb9238b1acf1908f6b6da169bb859b6849b822
Dalton <n7dalton@gmail.com>
2023-04-05 08:53:54 -0500

    Initial commit

---
+++ Methods_Lumpking_Dalton/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Methods_Lumpking_Dalton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("would you like to find a new number(n) or come into my house?(h)");
+            string twoOptions = Console.ReadLine();
+            if (twoOptions == "n")
+            {
+                Numbers();
+            }
+            if (twoOptions == "h")
+            {
+                enterHouse();
+            }
+
+            void enterHouse()
+            {
+                Console.WriteLine("Whats your name??");
+                string name = Console.ReadLine();
+                Console.WriteLine($"Welcome {name} to my lovely home!");
+                Console.WriteLine("now go away.");
+            }
+            void Numbers()
+            {
+                Console.WriteLine("whats your age");
+                int Age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("whats your fav number?");
+                int favNum = Convert.ToInt32(Console.ReadLine());
+                int multipliedNum = Age * favNum;
+                Console.WriteLine($"your new lucky number is....{multipliedNum}!! now get out of my coffee shop.");
+            }
+           
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Methods_Lumpking_Dalton
+ 
